f649325492234d811e0f759a58a75cc25181534d
Elizabeth Tadvick <etadvic0@email.cpcc.edu>
2021-03-08 20:13:31 -0500

    Descriptive
    Added an extra print line.  New line prints This is my time

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,10 @@
 public class HelloWorld
 {
 public static void main(String[] args)
-{
-System.out.println("Hello World!");
-}
+   {
+      System.out.println("Hello World!");
+
+      System.out.println("This is my time");
+      //Program modified, but able to revert to orignial version using GitHub Desktop
+   }
 }
