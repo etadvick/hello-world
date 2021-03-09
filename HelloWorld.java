1b5f58b3bd9d6ebdda2d218fbba82401ada6e70d
Elizabeth Tadvick <etadvic0@email.cpcc.edu>
2021-03-08 20:14:48 -0500

    Revert "Descriptive"
    This reverts commit f649325492234d811e0f759a58a75cc25181534d.

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,10 +3,7 @@
 public class HelloWorld
 {
 public static void main(String[] args)
-   {
-      System.out.println("Hello World!");
-
-      System.out.println("This is my time");
-      //Program modified, but able to revert to orignial version using GitHub Desktop
-   }
+{
+System.out.println("Hello World!");
+}
 }
